8a687a5112561b0f05d4f4153097012d8bb1aab4
Kiran <kiran@ProfPhillet.staff.sydney.edu.au>
2019-09-27 21:42:26 +1000

    resnet2

--- train_models.sh
+++ train_models.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
+
 GPUS=0,1
 STEPS=500
 LR=0.01
 LR_P=0.2
 BATCH_SIZE=128
-
 DATA_DIR="/home/kiran/radio_modulation/pytorch/new/pytorch_mod"
-
 
 #run binary classifier
 python3 run_net_pkl.py --train --file_train $DATA_DIR/qam_data.pkl --file_test $DATA_DIR/qam_data_test.pkl --steps $STEPS --batch_size $BATCH_SIZE --learning_rate $LR --results "train_results.csv" --filts 64,64,64,64,64,64,64,512,512,2 --model_path $DATA_DIR/model.pt --gpus $GPUS
